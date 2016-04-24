93afaaa8306c1374bf5f41aa2ca3e3d1ed211507
westinn <nicolaswestin@gmail.com>
2016-04-23 20:34:42 -0400

    Added dynamic icon and state checking for ending

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,33 @@
 var myAudio = new Audio(),
     onState = false;
 const NUM_OF_FILES = 60;
-myAudio.volume = .60;
+const VOLUME = 0.5;
+
+myAudio.volume = VOLUME;
 
 function pickSong() {
     var min = 0;
     index = Math.floor(Math.random() * (NUM_OF_FILES - min + 1) + min);
-    return 'music/' + index + '.mp3';
+    return '/music/' + index + '.mp3';
+}
+
+function changeIcon(state) {
+    // On and want to change to play symbol
+    if (state) {
+        chrome.browserAction.setIcon({
+            path: {
+                "38": "/images/icon38play.png"
+            }
+        });
+    }
+    // Off and want to change to default
+    else {
+        chrome.browserAction.setIcon({
+            path: {
+                "38": "/images/icon38.png"
+            }
+        });
+    }
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
@@ -14,20 +35,22 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     if (!onState) {
         myAudio.src = pickSong();
         myAudio.currentTime = 0;
-        myAudio.volume = .60;
         myAudio.play();
         onState = true;
+        changeIcon(onState);
     }
     // On and clicked
     else {
         myAudio.pause();
         onState = false;
+        changeIcon(onState);
     }
 });
 
 myAudio.addEventListener("ended", function() {
-    myAudio.src = pickSong();
-    myAudio.currentTime = 0;
-    myAudio.volume = .60;
-    myAudio.play();
+    if (state) {
+        myAudio.src = pickSong();
+        myAudio.currentTime = 0;
+        myAudio.play();
+    }
 });
